81ee823f2d56d37ed7179c8166e1875f5c239128
Michael <michael.w.kolb@gmail.com>
2023-02-06 13:48:56 -0500

    Completed assignment

--- src/features/users/UserInput.js
+++ src/features/users/UserInput.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
 function UserInput() {
+
   const dispatch = useDispatch();
 
   const [formData, setFormData] = useState({
--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+
+const users = useSelector((state) => state.users)
+
+const usersData = () => {
+  return users.map((user) => user.username)
+}
+
+const length = users.length
+
   return (
     <div>
       <ul>
         Users!
+        {usersData()}
+        Total Users: {length}
         {/* Write code here that displays the usernames of all users in the Redux store */}
         {/* In addition, display the total number of users curently in the store */}
       </ul>
